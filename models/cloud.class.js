13868a848483f7a463cb48f8d66ba52dccadbe49
Dennis Dumin <164201997+DennisDumin@users.noreply.github.com>
2025-02-12 00:24:37 +0100

    - Video 08 Spiellogik

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,4 +1,4 @@
-class Cloud extends MovableObject { 
+class Cloud extends MovableObject {
     y = 50;
     width = 400;
     height = 250;
@@ -8,7 +8,12 @@ class Cloud extends MovableObject {
         super().loadImage('img/5_background/layers/4_clouds/1.png')
 
         this.x = Math.random() * 500;
-        
+        this.animate();
+
     }
 
+    animate() {
+        this.moveLeft();
+    }
 }
+
--- models/world.class.js
+++ models/world.class.js
@@ -11,7 +11,11 @@ class World {
     ];
 
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png')
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+
     ]
 
     canvas;
@@ -20,15 +24,16 @@ class World {
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw();
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.addToMap(this.character);
         this.addObjectsToMap(this.backgroundObjects);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
+        this.addToMap(this.character);
         let self = this;
         requestAnimationFrame(function () {
             self.draw();
